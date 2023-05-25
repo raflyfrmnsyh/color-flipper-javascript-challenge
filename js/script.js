9ab9ea59f32d3b19924045534c6583e8afef79ca
raflyfrmnsyh <raflyfirmansyah017@gmail.com>
2023-05-25 17:56:35 +0700

    final commit

--- js/script.js
+++ js/script.js
@@ -1,27 +1,38 @@
-const colors = [
-    "116D6E",
-    "321E1E",
-    "4E3636",
-    "CD1818",
-    "F2D8D8",
-    "5C8984"
+const hex = [
+    "0","1","2","3","4","5","6","7","8","9",
+    "A","B","C","D","E","F"
 ];
 
 
 const buttonFlipper = document.querySelector('.btn-flipper');
 const backgroundFlipper = document.querySelector('._part-background');
 const hexText = document.querySelector('.text-hex');
+const overlay = document.querySelector('.overlay');
 
 buttonFlipper.addEventListener('click', () => {
-    const rondomNumber = getNumber();
-    console.log(rondomNumber);
 
-    backgroundFlipper.style.backgroundColor = `#${colors[rondomNumber]}`;
-    hexText.innerHTML = `#${colors[rondomNumber]}`;
+    let hexColor = "#";
+    for (let i = 0; i < 6; i++) {
+        hexColor += hex[getNumber()];
+    }
+
+    backgroundFlipper.style.backgroundColor = hexColor;
+    hexText.innerHTML = hexColor;
 
 });
 
+overlay.addEventListener('click', () => {
+    let text = hexText.innerHTML;
+
+    if(text !== "Default"){
+        navigator.clipboard.writeText(text);
+        alert('Success Copy to Clipboard')
+    }else{
+        return false;
+    }
+})
+
 function getNumber(){
-    let random = Math.random()*colors.length;
+    let random = Math.random()*hex.length;
     return Math.floor(random);
 }
